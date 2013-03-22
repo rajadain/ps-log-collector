af4c70a8d0b0b20ba155a80fd7d242d8c5cb67c4
Terence Tuhinanshu <terence@tuhinanshu.com>
2013-03-21 22:21:24 -0400

    Parametrize
    Use parameters instead of hardcoded paths

--- log-collector.ps1
+++ log-collector.ps1
@@ -1,31 +1,30 @@
+param(
+    [string]$source = "C:\inetpub\logs\LogFiles",
+    [string]$target = "."
+    )
+
 Import-Module WebAdministration # All the IIS control goodness
-
-# Initialize default destination directory for now
-$DestinDirectory = "C:\Users\Ttuhinanshu\myLogs"
-
-# Initialize default source directory
-$SourceDirectory = "C:\inetpub\logs\LogFiles"
 
 foreach ($site in Get-Website) { # Every site configured in IIS
     $ID = $site.ID
 
     # Assemble log file path
-    $LogFilePath = $SourceDirectory + "\W3SVC" + $ID
+    $LogFilePath = $source + "\W3SVC" + $ID
     
     # If the log file path exists
     if (Test-Path $LogFilePath) {
         # Assemble destination path
-        $Target = $DestinDirectory + "\" + $site.Name
+        $destin = $target + "\" + $site.Name
         
         # Create a directory with site name in destination folder if one doesn't exist already
-        if (!(Test-Path $Target)) {
-            New-Item $Target -type directory
+        if (!(Test-Path $destin)) {
+            New-Item $destin -type directory
         }
         
-        # Copy all logs files to target
-        robocopy $LogFilePath $Target
+        # Copy all logs files to destination
+        robocopy $LogFilePath $destin
         
         # Debug Info
-        "Copied " + (Get-ChildItem ($LogFilePath)).Count + " items from " + $LogFilePath + " to " + $Target
+        "Copied " + (Get-ChildItem ($LogFilePath)).Count + " items from " + $LogFilePath + " to " + $destin
     }
 }
